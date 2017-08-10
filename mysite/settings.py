a73a13e60071dc75d9428da90bdecaa3bedd7190
pixel <droid.akshay@gmail.com>
2017-08-10 20:17:31 +0530

    Allowed hosts updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o9^ni+nd1cmxogud1ziyp770q^x=s4uu4qw&sc(f2+2jl)5*_0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pixelexel.pythonanywhere.com']
 
 
 # Application definition
