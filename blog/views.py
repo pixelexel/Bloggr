9a2c0d8f0ae0e7e8630262d1e3216a6d0d72d9c0
pixel <droid.akshay@gmail.com>
2017-08-11 22:04:05 +0530

    Added homepage HTML

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
