bde5ef3079efc898645ae379b9826094f820d0b8
pixel <droid.akshay@gmail.com>
2017-08-22 15:47:05 +0530

    Added post list view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 url(r'^$', views.index, name = 'index'),
 url(r'^about/$', views.about, name = 'about'),
 url(r'^contact/$', views.contact, name = 'contact'),
-url(r'^post/new/$', views.post_new, name = 'post_new')
+url(r'^post/new/$', views.post_new, name = 'post_new'),
+url(r'^post/list/$', views.post_list, name = 'post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils import timezone
 
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
@@ -27,3 +28,7 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request, 'blog/post_new.html', {'form' : form})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
