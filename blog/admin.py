37a8819dc5593d06134542a831251a3fca64055a
pixel <droid.akshay@gmail.com>
2017-09-20 16:05:05 +0530

    Adding comments WIP

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
