e8cee5c68d301a14e4d5dbd337e6175c89b79175
pixel <droid.akshay@gmail.com>
2017-09-23 08:51:22 +0530

    Completed implementing Comments

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.utils import timezone
 
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 # Create your views here.
 
@@ -41,3 +41,26 @@ def post_detail(request, pk):
 def user_detail(request, user):
     posts = Post.objects.filter(published_date__lte = timezone.now(),  author__username = user).order_by('published_date') #To access foreign key relations use key__attribute # User is a string like author__username so only that can be compared!
     return render(request, 'blog/user_detail.html', {'posts' : posts, 'username' : user})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
