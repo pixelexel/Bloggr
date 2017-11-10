39aeb304baff2cb2123758479eafb8576c81818c
pixel <droid.akshay@gmail.com>
2017-11-10 15:00:50 +0530

    Version 0.2.2

--- blog/models.py
+++ blog/models.py
@@ -31,11 +31,6 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    # approved_comment = models.BooleanField(default=False)
-
-    # def approve(self):
-    #     self.approved_comment = True
-    #     self.save()
 
     def __str__(self):
         return self.text
